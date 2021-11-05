7490943184c28c09a3141670f85f011768009e03
Rodrigo <shimomurarodrigo1@gmail.com>
2021-11-05 00:54:06 -0300

    Start implementation of graphs

--- Project-4/src/Graph/graph.c
+++ Project-4/src/Graph/graph.c
@@ -1,31 +1,27 @@
 #include "graph.h"
 #include "../DynamicList/dynamicList.h"
+#include "../edge.h"
+#include "../vertex.h"
 
 typedef struct edge {
 
-    char* name;
     void* from;
     void* to;
-    char* left_side_square;
-    char* right_side_square;
-    double lenght;
-    double average_speed;
-
+    void* edge_data;
+    
 } edge;
 
 typedef struct vertex {
 
-    char* id;
-    void* edges;
-    double x;
-    double y;
+    void* edges; //List of edges
+    void* vertex_data; // Data
 
 } vertex;
 
 typedef struct graph {
     
-    vertex* vertexes;
-    int size;
+    vertex* vertexes; // Array of vertexes
+    int size, next_free_space;
 
 } graph;
 
@@ -39,36 +35,69 @@ void* create_graph(int size) {
 
     }
 
+    graph* aux = calloc(1, sizeof(graph));
+    aux->vertexes = calloc(size, sizeof(vertex));
+    aux->size = size;
+    aux->next_free_space = 0;
+
 }
 
-void* graph_find_vertex(void* connections, char* vertex_name) {
+void* create_vertex (char* id, double x, double y) {
+
+    vertex* aux = calloc(1, sizeof(vertex));
+    aux->vertex_data = new_vertex_data(id, x, y);
+    return aux;
+
+}
+
+void* graph_find_vertex(void* connections, char* vertex_id) {
 
     graph* graph_aux = connections;
 
-    for(int i = 0; i < graph_aux->size; i++) if(!strcmp(graph_aux->vertexes[i].id, vertex_name)) return &(graph_aux->vertexes[i]);
+    for(int i = 0; i < graph_aux->size; i++) if(!strcmp(vertex_get_id(graph_aux->vertexes[i].vertex_data), vertex_id)) return &(graph_aux->vertexes[i]);
 
     return NULL;
 
 }
 
-void graph_insert_edge(void* connections, char* vertex_1, char* vertex_2) {
+void graph_insert_vertex(void* connections, void* vertex_created) {
 
-    vertex* v1 = graph_find_vertex(connections, vertex_1);
-    vertex* v2 = graph_find_vertex(connections, vertex_2);
+    graph* aux_graph = connections;
 
-    if (!v1) {
-        printf("Vertex 1 with id = %s doesn't exists on the graph!\n", vertex_1);
+    if(aux_graph->next_free_space == aux_graph->size) {
+
+        puts("No more space available on the graph!");
+        return;
+
+    }
+
+    aux_graph->vertexes[aux_graph->next_free_space].vertex_data = vertex_created;
+    aux_graph->vertexes[aux_graph->next_free_space].edges = create_list();
+
+    aux_graph->next_free_space++;
+
+}
+
+void graph_insert_edge (void* connections, char* name, char* vertex_1, char* vertex_2, char* left_side_square, char* right_side_square, double length, double average_speed) {
+
+    graph* aux_graph = connections;
+
+    void* v1 = graph_find_vertex(connections, vertex_1);
+    void* v2 = graph_find_vertex(connections, vertex_2);
+
+    if(!v1) {
+        printf("Vertex 1 with id = %s doesn't exist on graph!\n", vertex_1);
         return;
     }
 
-    if (!v2) {
-        printf("Vertex 2 with id = %s doesn't exists on the graph!\n", vertex_2);
+    if(!v2) {
+        printf("Vertex 2 with id = %s doesn't exist on graph!\n", vertex_2);
         return;
     }
 
-    if(!v1->edges) v1->edges = create_list();
-
-
-
+    edge* aux_edge = calloc(1, sizeof(edge));
+    aux_edge->from = v1;
+    aux_edge->to = v2;
+    aux_edge->edge_data = new_edge_data(name, left_side_square, right_side_square, length, average_speed);
 
 }
