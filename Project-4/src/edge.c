146dea6e94bad66048994a6492e81e8bebefda8d
Rodrigo <shimomurarodrigo1@gmail.com>
2021-11-05 18:51:39 -0300

    Added edge direction (Todo, composed directions)

--- Project-4/src/edge.c
+++ Project-4/src/edge.c
@@ -1,4 +1,5 @@
 #include "edge.h"
+#include "vertex.h"
 
 typedef struct edge_data {
 
@@ -7,10 +8,11 @@ typedef struct edge_data {
     char* right_side_square;
     double length;
     double average_speed;
+    char direction[2];
 
 } edge_data;
 
-void* new_edge_data(char* name, char* left_side_square, char* right_side_square, double length, double average_speed) {
+void* new_edge_data(char* name, char* left_side_square, char* right_side_square, double length, double average_speed, void* vertex_1_data, void* vertex_2_data) {
 
     edge_data* aux = calloc(1, sizeof(edge_data));
     strcpy(aux->name, name);
@@ -18,6 +20,18 @@ void* new_edge_data(char* name, char* left_side_square, char* right_side_square,
     strcpy(aux->right_side_square, right_side_square);
     aux->length = length;
     aux->average_speed = average_speed;
+
+    if(vertex_get_x(vertex_1_data) == vertex_get_x(vertex_2_data)) {
+
+        vertex_get_y(vertex_1_data) > vertex_get_y(vertex_2_data) ? strcpy(aux->direction, "S") : strcpy(aux->direction, "N");
+        
+    } else if (vertex_get_y(vertex_1_data) == vertex_get_y(vertex_2_data)){
+
+        vertex_get_x(vertex_1_data) > vertex_get_x(vertex_2_data) ? strcpy(aux->direction, "W") : strcpy(aux->direction, "E");
+
+    }
+
+    //TODO NW, NE, SW, SE
 
 }
 
@@ -46,6 +60,11 @@ double edge_get_average_speed(void* data) {
     return aux->average_speed;
 }
 
+char* edge_get_direction(void* data) {
+    edge_data* aux = data;
+    return aux->direction;
+}
+
 void edge_set_name(void* data, char* name) {
     edge_data* aux = data;
     strcpy(aux->name, name);
@@ -69,4 +88,9 @@ void edge_set_length(void* data, double length) {
 void edge_set_average_speed(void* data, double length) {
     edge_data* aux = data;
     aux->length = length;
+}
+
+void edge_set_direction(void* data, char* direction) {
+    edge_data* aux = data;
+    strcpy(aux->direction, direction);
 }
--- Project-4/src/edge.h
+++ Project-4/src/edge.h
@@ -1,13 +1,15 @@
 #include "libs.h"
 
-void* new_edge_data(char* name, char* left_side_square, char* right_side_square, double length, double average_speed);
+void* new_edge_data(char* name, char* left_side_square, char* right_side_square, double length, double average_speed, void* vertex_1_data, void* vertex_2_data);
 char* edge_get_name(void* data);
 char* edge_get_left_side_square(void* data);
 char* edge_get_right_side_square(void* data);
 double edge_get_length(void* data);
 double edge_get_average_speed(void* data);
+char* edge_get_direction(void* data);
 void edge_set_name(void* data, char* name);
 void edge_set_left_side_square(void* data, char* left_side_square);
 void edge_set_right_side_square(void* data, char* right_side_square);
 void edge_set_length(void* data, double length);
 void edge_set_average_speed(void* data, double length);
+void edge_set_direction(void* data, char* direction);
