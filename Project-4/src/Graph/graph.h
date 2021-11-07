d2f183d38d81deb162887719fe96264d0d8e35ec
Rodrigo <shimomurarodrigo1@gmail.com>
2021-11-07 14:57:24 -0300

    Added graph functions prototypes

--- Project-4/src/Graph/graph.h
+++ Project-4/src/Graph/graph.h
@@ -1,1 +1,17 @@
 #include "libs.h"
+
+#ifndef GRAPH_H
+#define GRAPH_H
+
+
+void* create_graph(int size);
+void* create_vertex (char* id, double x, double y);
+void graph_insert_vertex(void* connections, void* vertex_created);
+void* graph_find_vertex(void* connections, char* vertex_id);
+void graph_insert_edge (void* connections, char* name, char* vertex_1, char* vertex_2, char* left_side_square, char* right_side_square, double length, double average_speed);
+void graph_remove_edge(void* connections, char* vertex_1, char* vertex_2);
+void* find_edge(void* v1_edges, void* v2);
+int adjacent(void* connections, char* vertex_1, char* vertex_2);
+void* list_of_adjacents(void* connections, char* vertex_1);
+
+#endif
