1ef0946c81c49ad25231185867f591be9734fc1c
Rodrigo <shimomurarodrigo1@gmail.com>
2021-11-07 14:33:54 -0300

    Updated dynamic list

--- Project-4/src/DynamicList/dynamicList.c
+++ Project-4/src/DynamicList/dynamicList.c
@@ -26,28 +26,29 @@ void* create_list() {
 
 }
 
-void insert_list(void* sequence, void* element) {
+void* insert_list(void* sequence, void* element) {
 
     list* list_aux = sequence;
-    node* pontAux = calloc(1, sizeof(node));
-    pontAux->element = element;
+    node* node_aux = calloc(1, sizeof(node));
+    node_aux->element = element;
 
     if (list_aux->size == 0) {
 
-        pontAux->next = NULL;
-        pontAux->prev = NULL;
-        list_aux->head = pontAux;
-        list_aux->end = pontAux;
+        node_aux->next = NULL;
+        node_aux->prev = NULL;
+        list_aux->head = node_aux;
+        list_aux->end = node_aux;
 
     } else {
 
-        pontAux->next = NULL;
-        list_aux->end->next = pontAux;
-        pontAux->prev = list_aux->end;
-        list_aux->end = pontAux;
+        node_aux->next = NULL;
+        list_aux->end->next = node_aux;
+        node_aux->prev = list_aux->end;
+        list_aux->end = node_aux;
     }
 
     list_aux->size++;
+    return node_aux;
 
 }
 
@@ -104,18 +105,18 @@ void free_list(void* sequence, bool remove_elements, void (*free_node)(void*)) {
 
     }
 
-    node* auxNode = list_aux->head;
-    node* auxElement = list_aux->head->element;
+    node* aux_node = list_aux->head;
+    node* aux_element = list_aux->head->element;
 
     while (list_aux->head != NULL) {
 
-        auxNode = list_aux->head;
-        auxElement = list_aux->head->element;
+        aux_node = list_aux->head;
+        aux_element = list_aux->head->element;
         list_aux->head = list_aux->head->next;
 
-        if(remove_elements == true) free_node(auxElement);
+        if(remove_elements == true) free_node(aux_element);
 
-        free(auxNode);
+        free(aux_node);
     }
 
     free(sequence);
@@ -134,7 +135,7 @@ void* at_pos(void* sequence, int index) {
 
 }
 
-void remove_node(void* sequence, void* node_to_remove, void (*free_node)(void*), bool remove_elements) {
+void* remove_node(void* sequence, void* node_to_remove, void (*free_node)(void*), bool remove_elements) {
     
     list* list_aux = sequence;
     node* aux1;
@@ -151,7 +152,7 @@ void remove_node(void* sequence, void* node_to_remove, void (*free_node)(void*),
         if (pos > list_aux->size) {
 
             puts("Not found.");
-            return;
+            return NULL;
 
         }
 
@@ -169,13 +170,17 @@ void remove_node(void* sequence, void* node_to_remove, void (*free_node)(void*),
 
         aux1 = list_aux->end;
         aux2 = aux1->prev;
+        void* temp = aux1->element;
 
-        if(remove_elements == true) free_node(aux1->element);
+        if(remove_elements) free_node(aux1->element);
+        
         free(aux1);
 
         aux2->next = NULL;
         list_aux->end = aux2;
         list_aux->size--;
+
+        if(!remove_elements) return temp;
 
     } else if (pos == 0 && list_aux->size > 1) {
         
@@ -183,18 +188,24 @@ void remove_node(void* sequence, void* node_to_remove, void (*free_node)(void*),
 
         aux1 = list_aux->head->next;
 
-        if(remove_elements == true) free_node(list_aux->head->element);
+        void* temp = list_aux->head->element;
+
+        if(remove_elements) free_node(list_aux->head->element);
 
         free(list_aux->head);
 
         list_aux->head = aux1;
         list_aux->size--;
 
+        if(!remove_elements) return temp;
+
     } else if (pos == 0 && list_aux->size == 1) {
 
         // Remove the only element of the list
 
-        if(remove_elements == true) free_node(list_aux->head->element);
+        void* temp = list_aux->head->element;
+
+        if(remove_elements) free_node(list_aux->head->element);
 
         free(list_aux->head);
 
@@ -202,6 +213,8 @@ void remove_node(void* sequence, void* node_to_remove, void (*free_node)(void*),
         list_aux->end = NULL;
 
         list_aux->size--;
+
+        if(!remove_elements) return temp;
 
     } else {
 
@@ -211,11 +224,20 @@ void remove_node(void* sequence, void* node_to_remove, void (*free_node)(void*),
         aux1->next = aux2->next;
         aux2->next->prev = aux1;
 
-        if(remove_elements == true) free_node(aux2->element);
+        void* temp = aux2->element;
+
+        if(remove_elements) free_node(aux2->element);
+
         free(aux2);
 
         list_aux->size--;
+
+        if(!remove_elements) return temp;
+
     }
+
+    return NULL;
+
 }
 
 void* find_element(void* sequence, void* match, int (*compare_info)(void*, void*)) {
